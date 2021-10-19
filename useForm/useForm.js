54b837bd7d72e8592e7acc2d2c09a73124956df9
Nicolás Arévalo <inag.458@gmail.com>
2021-10-19 00:06:36 -0500

    Segundo comit con las carpetas de Fernando Herrera

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-export const useForm = ( initialState  = {} ) =>{
-    const {values, setValues} = useState(initialState);
+
+export const useForm = ( initialState = {} ) => {
     
-    const reset = () =>{
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
         setValues( initialState );
     }
 
+
     const handleInputChange = ({ target }) => {
+
         setValues({
             ...values,
             [ target.name ]: target.value
         });
+
     }
 
     return [ values, handleInputChange, reset ];
+
 }
